aeec7fbc8c02b4e304d5ee2fb1b791b9f4c9dd33
gdsaikrishna <gdsaikrishna@gmail.com>
2022-04-05 10:50:32 +0530

    multiply strings

---
+++ 43.Multiply Strings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        #no1, no2 = 0,0
+        #for digit in num1:
+        #    no1 = no1*10 + (ord(digit)-ord('0'))
+        #for digit in num2:
+        #    no2 = no2*10 + (ord(digit)-ord('0'))
+        #return str(no1*no2)
+        return str(int(num1)*int(num2))
+        
