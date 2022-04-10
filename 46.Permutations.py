d73ee2ba2febc8353d3fd62fb4b6fc88a3f5c4c0
gdsaikrishna <gdsaikrishna@gmail.com>
2022-04-10 11:39:35 +0530

    permutations unique

--- 46.Permutations.py
+++ 46.Permutations.py
@@ -3,3 +3,16 @@ class Solution:
         return [[n] + p
             for i, n in enumerate(nums)
             for p in self.permute(nums[:i] + nums[i+1:])] or [[]]
+
+    # DFS
+# def permute(self, nums):
+#     res = []
+#     self.dfs(nums, [], res)
+#     return res
+    
+# def dfs(self, nums, path, res):
+#     if not nums:
+#         res.append(path)
+#         # return # backtracking
+#     for i in xrange(len(nums)):
+#         self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
