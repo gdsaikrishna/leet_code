5d7c634682fd1eb75e287840f890d69e8a15006c
gdsaikrishna <gdsaikrishna@gmail.com>
2022-04-30 13:42:52 +0530

    backspace op

---
+++ 844. Backspace String Compare.py
@@ -0,0 +1,16 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        s1, t1= [], []
+        for i in s:
+            if i == "#":
+                if len(s1) != 0:
+                    s1.pop()
+            else:
+                s1.append(i)
+        for i in t:
+            if i == "#":
+                if len(t1)!=0:
+                    t1.pop()
+            else:
+                t1.append(i)
+        return s1==t1
