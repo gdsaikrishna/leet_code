69832d70ec79522ddfac616fdea85fd488a046dd
gdsaikrishna <gdsaikrishna@gmail.com>
2022-05-12 18:35:12 +0530

    Array problems sols

---
+++ 2057. Smallest Index With Equal Value.py
@@ -0,0 +1,6 @@
+class Solution:
+    def smallestEqual(self, nums: List[int]) -> int:
+        for idx, n in enumerate(nums):
+            if idx % 10 == n:
+                return idx
+        return -1 
